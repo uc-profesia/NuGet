58ee9778d8ad7dc65acf12d7707b81228df2b4ee
Ugo <ugo.ciraci@profesia.it>
2016-11-02 17:13:52 +0100

    First commit

---
+++ NuGetConsoleApplication/NuGetConsoleApplication/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Base.NugetHello;
+
+namespace NuGetConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Begin: Base.ConsoleApplication.");
+            Example e = new Example();
+            e.WriteLicense();
+            Console.WriteLine(e.GetMessage());
+            Console.WriteLine("");
+            Console.WriteLine("End: Base.ConsoleApplication.");
+            Console.WriteLine("");
+            Console.WriteLine("Press anykey to exit.");
+            Console.ReadKey();
+       
+        }
+    }
+}
